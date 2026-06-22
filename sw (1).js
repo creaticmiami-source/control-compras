51266db429a2bdf09ebe72177e0f489288a757e9
creaticmiami-source <creatic.miami@gmail.com>
2026-06-21 23:05:41 -0400

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'chateau-gastos-v2';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-180.png',
+  './favicon-32.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Red primero (para que las actualizaciones se vean de inmediato).
+// Si no hay internet, usa la copia guardada como respaldo.
+// Todo lo que no sea del mismo dominio (Supabase, proxy de IA) pasa directo a la red.
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method !== 'GET' || !req.url.startsWith(self.location.origin)) return;
+
+  event.respondWith(
+    fetch(req)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(req, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(req))
+  );
+});
